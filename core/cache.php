e620777c54608d6ffb2e9a62709d58f7601025d6
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-01-27 00:33:44 +0100

    invalidate cache

--- core/cache.php
+++ core/cache.php
@@ -1,5 +1,8 @@
 <?php
 class Cache {
+	public $minQueries = 100;
+	public $maxQueries = 200; 
+	
 	private $path; 
 	
 	public function __construct() {
@@ -19,5 +22,24 @@ class Cache {
 	public function save($query, $result) {
 		$name = md5($query);
 		file_put_contents($this->path . $name . '.txt', $result);
+		$this->invalidate();
+	}
+	
+	private function invalidate() {
+		$files = glob($this->path . '*');
+		$filesTime = array();
+		$numberOfFiles = count($files);
+		if($numberOfFiles > $this->maxQueries) {
+			foreach ($files as $file) {
+				$filesTime[$file] = filemtime($file);
+			}
+		}
+		
+		asort($filesTime);
+		$oldFiles = array_slice($filesTime, 0, $numberOfFiles - $this->minQueries);
+		
+		foreach ($oldFiles as $filename => $val) {
+			unlink($filename);
+		}
 	}
 }
