47d46d7686f5009c4c34ed4672502dd5c144dd31
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-01-27 14:17:50 +0100

    refactoring

---
+++ core/sresults.php
@@ -0,0 +1,38 @@
+<?php
+class Sresults {
+	public $totalLinks;
+	public $linksOnPage;
+	
+	private $documents;
+	private $currentPage;
+	
+	public function __construct($results, $page = 1) {
+		$this->documents = $results->documents;
+		$this->totalLinks = count($this->documents);
+		$this->currentPage = getGETValue('page', $page);
+	}
+	
+	public function getLinksList() {
+		$from = ($this->currentPage - 1) * $this->linksOnPage; 
+		
+	    $html = '<ol>';
+	    foreach (array_slice($this->documents, $from, $this->linksOnPage) as $document) {
+	        $html .= $this->getItem($document);
+	    }
+	    $html .= '</ol>';
+		
+	    return $html;
+	}
+	
+	public function getPaginator() {
+		$paginator = new Paginator($this->totalLinks);
+		$steps = $paginator->getList($this->currentPage);
+		return $steps;
+	}
+	
+	private function getItem($document) {
+		$url = $document->url;
+        $title = $document->title;
+        return getLink($url, $title, 'li');
+	}
+}
