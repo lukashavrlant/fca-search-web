9eb2635c8d83110867cf51d81ca31eb8e21f3dc1
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-02-16 11:10:53 +0100

    path changed

--- constants.php
+++ constants.php
@@ -4,7 +4,7 @@ if (LOCALHOST) {
 	$fcasearch = '/Users/lukashavrlant/Python/fca-search/src/search ';
 	$python3 = '/Library/Frameworks/Python.framework/Versions/3.2/bin/python3 ';
 } else {
-	$fcasearch = '/home/havrlanl/Chandler/src/search ';
+	$fcasearch = '/home/havrlanl/fca-search/src/search ';
 	$python3 = '/home/havrlanl/python-3.2.2/bin/python3 ';
 }
 
